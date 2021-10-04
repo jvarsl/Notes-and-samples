9d8d2ffb1cca0344dd6d44f5f50175693254d1cf
jvarsl <j.varslauskas@gmail.com>
2021-10-04 23:21:52 +0300

    angular 1pt

---
+++ ASPNET_MVC_Framework_EntityCRUD_2/angular-task-tracker/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', //html tag to embed component
+  templateUrl: './app.component.html', //just out html file we're using
+  styleUrls: ['./app.component.css'] //just css, can be more than one
+})
+export class AppComponent {
+}
